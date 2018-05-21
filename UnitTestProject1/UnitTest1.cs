90f71c528a5f4bbb2f613a8a08dfad76fb652103
LucasSq <lucas.sikking@gmail.com>
2018-05-21 12:12:06 +0200

    restructured unit tests

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,46 +8,12 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-        public void TestMaxLength(int maxlength, string fieldname)
-        {
-
-            DataValidator datavalidator1 = new DataValidator();
-            DataGenerator datagenerator1 = new DataGenerator();
-            ExpectationGenerator expectationgenerator1 = new ExpectationGenerator();
-            List<string> testData = datagenerator1.generateStringDataBasedOnLength(maxlength);
-            List<string> testExpectation = expectationgenerator1.generateExpactationsAsList();
-            int count = 0;
-
-            foreach (var item in testData)
-            {
-                datavalidator1.validateStringField(testData[count]);
-                Assert.AreEqual(testExpectation[count], datavalidator1.message);
-                Console.WriteLine("if I fill " + fieldname + " (max length = " + maxlength + ") field with value " + testData[count] + " length = " + testData[count].Length + " results in messages: " + testExpectation[count]);
-                count += 1;
-            }
-        }
-
-        public void TestMaxLengthWithTestDataObject(int maxlength, string fieldname)
-        {
-
-           DataValidator datavalidator1 = new DataValidator();
-           DataGenerator datagenerator1 = new DataGenerator();
-           List<TestData> testData = datagenerator1.GenerateStringDataAsObjectList(maxlength);
-           int count = 0;
-
-            foreach (var item in testData)
-            {
-                datavalidator1.validateStringField(testData[count].input);
-                Assert.AreEqual(testData[count].expectation, datavalidator1.message);
-                Console.WriteLine("if I fill " + fieldname + " (max length = " + maxlength + ") field with value " + testData[count] + " length = " + testData[count].input.Length + " results in messages: " + testData[count].expectation);
-                count += 1;
-            }
-        }
+        UnitTestTemplates unitTestTemplates = new UnitTestTemplates();
 
         [TestMethod]
         public void TestCheckFieldNaam()
         {
-            TestMaxLength(11, "naam");
+           unitTestTemplates.TestMaxLength(10, "naam");
         }
 
         
@@ -59,7 +25,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestCheckFieldNaamWithTestDataAsObjectList()
         {
-            TestMaxLengthWithTestDataObject(10, "naam");
+            unitTestTemplates.TestMaxLengthWithTestDataObject(10, "naam");
         }
 
     }
---
+++ UnitTestProject1/UnitTestTemplates.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GetRekt;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestTemplates
+    {
+        public void TestMaxLength(int maxlength, string fieldname)
+        {
+
+            DataValidator datavalidator1 = new DataValidator();
+            DataGenerator datagenerator1 = new DataGenerator();
+            ExpectationGenerator expectationgenerator1 = new ExpectationGenerator();
+            List<string> testData = datagenerator1.generateStringDataBasedOnLength(maxlength);
+            List<string> testExpectation = expectationgenerator1.generateExpactationsAsList();
+            int count = 0;
+
+            foreach (var item in testData)
+            {
+                datavalidator1.validateStringField(testData[count]);
+                Assert.AreEqual(testExpectation[count], datavalidator1.message);
+                Console.WriteLine("if I fill " + fieldname + " (max length = " + maxlength + ") field with value " + testData[count] + " length = " + testData[count].Length + " results in messages: " + testExpectation[count]);
+                count += 1;
+            }
+        }
+
+        public void TestMaxLengthWithTestDataObject(int maxlength, string fieldname)
+        {
+
+            DataValidator datavalidator1 = new DataValidator();
+            DataGenerator datagenerator1 = new DataGenerator();
+            List<TestData> testData = datagenerator1.GenerateStringDataAsObjectList(maxlength);
+            int count = 0;
+
+            foreach (var item in testData)
+            {
+                datavalidator1.validateStringField(testData[count].input);
+                Assert.AreEqual(testData[count].expectation, datavalidator1.message);
+                Console.WriteLine("if I fill " + fieldname + " (max length = " + maxlength + ") field with value " + testData[count] + " length = " + testData[count].input.Length + " results in messages: " + testData[count].expectation);
+                count += 1;
+            }
+        }
+
+    }
+
+
+}
